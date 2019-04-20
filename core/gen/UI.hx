2a9f382a3486f5b49e499163cba43fbb931d3759
Jeremy <cream@sourcre.am>
2019-04-20 09:52:40 -0700

    formatting

--- core/gen/UI.hx
+++ core/gen/UI.hx
@@ -7,13 +7,13 @@ import gen.GenUpdate;
 import gen.GenModel;
 
 class UI {
-	public static function collapsingWindow(arch:Architecture<GenModel, GenMsg>, window :WindowContent, children :Array<VirtualNode>):VirtualNode {
+    public static function collapsingWindow(arch:Architecture<GenModel, GenMsg>, window :WindowContent, children :Array<VirtualNode>):VirtualNode {
         var heightClass = window.isOpen ? " full-height" : "";
         var arrow = window.isOpen ? "▼ " : "▶ ";
 
-		return arch.div([CLASS("column-item border-bottom" + heightClass)], [
+        return arch.div([CLASS("column-item border-bottom" + heightClass)], [
             arch.h1([CLASS("column-item-text"), ON_CLICK(ToggleWindow(window))], arrow + window.title),
             arch.div([CLASS("column-item-content")], children)
         ]);
-	}
+    }
 }
