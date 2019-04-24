1b8921e640ee36b11689733453b84207674db5cf
Jeremy <cream@sourcre.am>
2019-04-23 19:30:09 -0700

    pongo updated to use inc dom

--- core/gen/view/GenView.hx
+++ core/gen/view/GenView.hx
@@ -1,15 +1,15 @@
 package gen.view;
 
 using iqua.Html;
-import iqua.Lazy;
+import iqua.RenderFunction;
 using gen.view.UI;
-import iqua.VirtualNode;
 import iqua.Architecture;
 import gen.update.GenMsg;
 import gen.model.GenModel;
 
 class GenView {
-	public static function view(arch:Architecture<GenModel, GenMsg>, model:GenModel):VirtualNode {
+	public static function view(arch:Architecture<GenModel, GenMsg>, model:GenModel) : RenderFunction 
+	{
 		return arch.div([CLASS("full-screen"), MOUSE_DOWN(null,GlobalDown), MOUSE_UP(null,GlobalUp), MOUSE_MOVE(null,GlobalMove)], [
 			arch.div([CLASS("nav-bar border-bottom")], [
 				arch.pushButton(model.button1, [arch.p([], "Hello")])
@@ -22,19 +22,13 @@ class GenView {
 					arch.div([CLASS("game-window")], []),
 					arch.bottom(model.bottom, [
 						arch.pushButton(model.button2, [arch.p([], "Hello")]),
-						arch.inputText(model.text),
-						Lazy.lazy1("regTest", cool)(arch, model.text.data)
+						arch.inputText(model.text)
 					])
 				]),
 				arch.column(model.columnRight ,[for (window in model.columnRight.windows) arch.collapsingWindow(window, [
 
-				])]),
+				])])
 			])
 		]);
 	}
-
-	public static function cool(arch:Architecture<GenModel, GenMsg>, str :String):VirtualNode {
-		return arch.p([], str);
-	}
-	
 }
--- core/gen/view/UI.hx
+++ core/gen/view/UI.hx
@@ -2,7 +2,6 @@ package gen.view;
 
 using iqua.Html;
 using gen.view.UI;
-import iqua.VirtualNode;
 import iqua.Architecture;
 import gen.update.GenMsg;
 import gen.model.GenModel;
@@ -10,9 +9,11 @@ import gen.model.Column;
 import gen.model.Button;
 import gen.model.Text;
 import gen.model.WindowContent;
+import iqua.RenderFunction;
 
 class UI {
-    public static function collapsingWindow(arch:Architecture<GenModel, GenMsg>, window :WindowContent, children :Array<VirtualNode>):VirtualNode {
+    public static function collapsingWindow(arch:Architecture<GenModel, GenMsg>, window :WindowContent, children :Array<RenderFunction>) : RenderFunction 
+    {
         var heightClass = window.isOpen ? " open" : "";
         var arrow = window.isOpen ? "▼ " : "▶ ";
 
@@ -25,18 +26,19 @@ class UI {
         return arch.div([CLASS("collapse border-bottom" + heightClass)], content);
     }
 
-    public static function column(arch:Architecture<GenModel, GenMsg>, column :Column, children :Array<VirtualNode>):VirtualNode {
+    public static function column(arch:Architecture<GenModel, GenMsg>, column :Column, children :Array<RenderFunction>) : RenderFunction 
+    {
         var openClass = column.isOpen ? " open" : " closed";
         var content = column.isOpen 
             ? arch.div([CLASS("flex-column column-content border-left border-right")], children)
-            : null;
+            : arch.div([], []);
         var leftClass = column.isLeft ? " left" : "";
         var activeClass = column.isActive ? " active" : "";
 
         var innerConent = [
             arch.h1([CLASS("column-collapser toggler" + activeClass), MOUSE_DOWN(column,StretchColumn), ON_DBL_CLICK(ToggleColumn(column))], "⋮"),
             content,
-            arch.div([CLASS("column-collapser barrier")], null)
+            arch.div([CLASS("column-collapser barrier")], [])
         ];
         if(column.isLeft) {
             innerConent.reverse();
@@ -45,18 +47,20 @@ class UI {
         return arch.div([CLASS("column flex-row border-right border-left" + openClass + leftClass), STYLE({width: column.width + "px"})], innerConent);
     }
 
-    public static function bottom(arch:Architecture<GenModel, GenMsg>, bottom :WindowContent, children :Array<VirtualNode>):VirtualNode {
+    public static function bottom(arch:Architecture<GenModel, GenMsg>, bottom :WindowContent, children :Array<RenderFunction>) : RenderFunction 
+    {
         var openClass = bottom.isOpen ? " open" : " closed";
         return arch.div([CLASS("bottom-row border-top" + openClass)], [arch.collapsingWindow(bottom, children)]);
     }
 
-    public static function pushButton(arch:Architecture<GenModel, GenMsg>, button :Button, children :Array<VirtualNode>):VirtualNode {
+    public static function pushButton(arch:Architecture<GenModel, GenMsg>, button :Button, children :Array<RenderFunction>) : RenderFunction 
+    {
         var activeClass = button.isActive ? " active" : "";
         return arch.div([CLASS("button" + activeClass), ON_CLICK(ToggleButton(button))], children);
     }
 
-    public static function inputText(arch:Architecture<GenModel, GenMsg>, text :Text):VirtualNode {
-
+    public static function inputText(arch:Architecture<GenModel, GenMsg>, text :Text) : RenderFunction 
+    {
         return arch.input([VALUE(text.data), ON_INPUT(text, TextInput)]);
     }
 }
