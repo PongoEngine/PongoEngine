379988c33c0530d444b2ec798fb54fd578c9a1b1
Jeremy <cream@sourcre.am>
2019-04-28 20:23:50 -0700

    remove material

--- core/Main.hx
+++ core/Main.hx
@@ -4,11 +4,13 @@ import towser.Html.*;
 import perdita.Material.*;
 import perdita.model.Textfield;
 import perdita.model.Tab;
+import gen.model.GenModel;
+import gen.update.GenUpdate;
+import gen.view.GenView;
 
 class Main {
 	static function main() {
-		// new Architecture("app", GenUpdate.update, GenView.view, new GenModel());
-		new Architecture("app", Main.update, Main.view, new PongoModel());
+		new Architecture("app", GenUpdate.update, GenView.view, new GenModel());
 		hotModule();
 	}
 
@@ -19,95 +21,4 @@ class Main {
 			});
 		}
 	}
-
-	public static function view(model:PongoModel) : RenderFunction<PongoModel, PongoMsg>
-	{
-		return div([CLASS("full-screen")], [
-			tooltip("Open ⌘ + O"),
-			div([STYLE({marginTop: "10px"})], [
-				textFieldFilled(UPDATE_TEXT, model.moneyField)
-			]),
-			div([STYLE({marginTop: "10px"})], [
-				textFieldOutlined(UPDATE_TEXT, model.moneyField)
-			]),
-			div([STYLE({marginTop: "10px"})], [
-				fixedTabs(CLICK_TAB, model.tabs)
-			]),
-			div([STYLE({marginTop: "10px", width: "300px"})], [
-				scrollableTabs(CLICK_TAB, model.tabs)
-			]),
-			div([STYLE({marginTop: "10px"})], [
-				snackbar([
-					span([], 'Saved in "Wedding" album')
-				])
-			]),
-			div([STYLE({marginTop: "10px"})], [
-				sliderContinuous(SLIDER_CHANGE, 0, 2)
-			]),
-			div([STYLE({marginTop: "10px"})], [
-				sliderDiscrete(SLIDER_CHANGE, 0, 200, 10)
-			]),
-			// div([STYLE({marginTop: "10px"})], [
-			// 	sheetSide(false, content("Right side", 40))
-			// ]),
-			// div([STYLE({marginTop: "10px"})], [
-			// 	sheetSide(true, content("Right side", 40))
-			// ]),
-			// div([STYLE({marginTop: "10px"})], [
-			// 	sheetBottom(content("Bottom", 40))
-			// ]),
-			div([STYLE({marginTop: "10px"})], [
-				progressCircular()
-			]),
-
-		]);
-	}
-
-	public static function content(title :String, lines : Int) : Array<RenderFunction<PongoModel, PongoMsg>>
-	{
-		return [
-			h1([], title)
-		].concat([for(i in 0...lines) h4([], "Hello content")]);
-	}
-
-	public static function update(msg:PongoMsg, model:PongoModel):Bool {
-		switch msg {
-			case UPDATE_TEXT(text):
-				model.moneyField.value = text;
-			case CLICK_TAB(tabs, tab):
-				for(t in tabs) t.isActive = false;
-				tab.isActive = true;
-			case SLIDER_CHANGE(val):
-				trace(val);
-		}
-		return true;
-	}
-}
-
-class PongoModel
-{
-	public var moneyField :Textfield;
-	public var tabs :Array<Tab>;
-
-	public function new() : Void
-	{
-		this.moneyField = new Textfield("Money", "");
-		this.tabs = [
-			new Tab(false, "Apple"),
-			new Tab(false, "Pear"),
-			new Tab(false, "Orange"),
-			new Tab(false, "Carror"),
-			new Tab(false, "Blackberry"),
-			new Tab(false, "Mango"),
-			new Tab(false, "Grape"),
-			new Tab(false, "Celery")
-		];
-	}
-}
-
-enum PongoMsg
-{
-	UPDATE_TEXT(text :String);
-	SLIDER_CHANGE(val :String);
-	CLICK_TAB(tabs :Array<Tab>, tab :Tab);
 }
--- lib/Perdita/src/haxe/perdita/Material.hx
+++ lib/Perdita/src/haxe/perdita/Material.hx
@@ -9,98 +9,6 @@ import perdita.model.Tab;
 
 class Material
 {
-    //------------ PROGRESS -----------
-    public static function progressLinear<Model, Msg>() :RenderFunction<Model, Msg>
-    {
-        return div([CLASS("m-progress-linear")], []);
-    }
-
-    public static function progressCircular<Model, Msg>() :RenderFunction<Model, Msg>
-    {
-        return svg([CLASS("m-progress-circular")], [
-            circle([CLASS("m-progress-circular-path"), CX("50"), CY("50"), R("20"), FILL("none"), STROKE_WIDTH("3"), STROKE_MITER_LIMIT("10")])
-        ]);
-    }
-
-    //------------ SHEETS -----------
-    public static function sheetSide<Model, Msg>(isLeft :Bool, children :Array<RenderFunction<Model, Msg>>) :RenderFunction<Model, Msg>
-    {
-        var posClass = isLeft ? " left" : " right";
-        return div([CLASS("m-sheet-side" + posClass)], children);
-    }
-
-    public static function sheetBottom<Model, Msg>(children :Array<RenderFunction<Model, Msg>>) :RenderFunction<Model, Msg>
-    {
-        return div([CLASS("m-sheet-bottom")], children);
-    }
-
-
-    //------------ SLIDERS -----------
-    public static function sliderContinuous<Model, Msg>(msg :String -> Msg, min :Int, max :Int) :RenderFunction<Model, Msg>
-    {
-        var step = (max - min) / 500;
-        return input([
-            ON_CHANGE(msg),
-            ON_INPUT(msg),
-            TYPE("range"), 
-            ATTR("min", min), 
-            ATTR("max", max), 
-            ATTR("step", step), 
-            CLASS("m-slider-continuous")
-        ]);
-    }
-
-    public static function sliderDiscrete<Model, Msg>(msg :String -> Msg, min :Int, max :Int, step :Int) :RenderFunction<Model, Msg>
-    {
-        var stepCount = (max-min) * step;
-        return div([], [
-            input([
-                ON_CHANGE(msg),
-                ON_INPUT(msg),
-                TYPE("range"), 
-                ATTR("min", min), 
-                ATTR("max", max), 
-                ATTR("step", step), 
-                ATTR("list", "steplist"), 
-                CLASS("m-slider-continuous")
-            ]),
-            datalist([ID("steplist")], [
-                for(i in 0...stepCount) option([], i + "")
-            ])
-        ]);
-    }
-
-    //------------ SNACKBARS -----------
-    public static function snackbar<Model, Msg>(children :Array<RenderFunction<Model, Msg>>) :RenderFunction<Model, Msg>
-    {
-        return div([CLASS("m-snackbar")], children);
-    }
-
-    //------------ TABS -----------
-    public static function fixedTabs<Model, Msg>(msg :Array<Tab> -> Tab -> Msg, tabs :Array<Tab>) :RenderFunction<Model, Msg>
-    {
-        return div([], [
-            for(tab in tabs) span([CLASS("m-tab" + (tab.isActive ? " active" : "")), ON_CLICK(msg(tabs, tab))], tab.value)
-        ]);
-    }
-
-    public static function scrollableTabs<Model, Msg>(msg :Array<Tab> -> Tab -> Msg, tabs :Array<Tab>) :RenderFunction<Model, Msg>
-    {
-        return div([CLASS("m-tab-scrollable")], [
-            for(tab in tabs) span([CLASS("m-tab" + (tab.isActive ? " active" : "")), ON_CLICK(msg(tabs, tab))], tab.value)
-        ]);
-    }
-
-    //------------ TEXTFIELDS -----------
-    public static function textFieldFilled<Model, Msg>(msg :String -> Msg, field :Textfield) :RenderFunction<Model, Msg>
-    {
-        var filledClass = field.value == "" ? "" : " filled";
-        return div([CLASS("m-textfield-filled" + filledClass)], [
-            input([ON_INPUT(msg), VALUE(new String(field.value))]),
-            span([], field.label)
-        ]);
-    }
-
     public static function textFieldOutlined<Model, Msg>(msg :String -> Msg, field :Textfield) :RenderFunction<Model, Msg>
     {
         var filledClass = field.value == "" ? "" : " filled";
@@ -108,11 +16,5 @@ class Material
             input([ON_INPUT(msg), VALUE(new String(field.value))]),
             span([], field.label)
         ]);
-    }
-
-    //------------ TOOLTIP -----------
-    public static function tooltip<Model, Msg>(tip :String) :RenderFunction<Model, Msg>
-    {
-        return div([CLASS("m-tooltip m-fade-in")], [span([], tip)]);
     }
 }
